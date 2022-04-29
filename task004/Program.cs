4d30a79b2c4bd5936ce8ee34b4c215eaba3642b2
ruslan <rus.pro94@bk.ru>
2022-04-29 20:59:32 +0500

    T004

---
+++ task004/Program.cs
@@ -0,0 +1,6 @@
+﻿/* 5. Напишите прогр вычисления значения функции возведения числа в квадрат.*/
+
+
+Console.WriteLine(" Пожалуйста введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);
