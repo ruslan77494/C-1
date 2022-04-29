3d27212d44ec2c970d8bc41828c7d871e104b4e7
ruslan <rus.pro94@bk.ru>
2022-04-29 21:35:11 +0500

    T005

---
+++ task005/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите прогу которая на вход принимает одно число (N) а на выходе показывает все целые числа в промежутке от -N ДО N*/
+
+Console.WriteLine(" Пожалуйста введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = - number; i <= number; i++)
+{
+    Console.Write(i);
+    if (i != number)
+    {
+        Console.Write(",");
+    }
+}
+
+
+
