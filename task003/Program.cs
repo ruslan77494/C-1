9b8493b216b011293e59777389472d699df47e17
ruslan <rus.pro94@bk.ru>
2022-04-29 20:39:57 +0500

    T003

---
+++ task003/Program.cs
@@ -0,0 +1,33 @@
+﻿/*3. Напишите программу,которая будет выдавать название дня недели по зодонному номеру.
+3-> cреда
+5-> пятница
+*/
+
+Console.WriteLine("Пожалуста Введите число:  ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+      case 1:
+      {
+             System.Console.WriteLine("Пн");
+             break;
+      } 
+      case 2:
+     {
+             System.Console.WriteLine("Вт");
+             break;
+     }
+     case 3:
+           {
+             System.Console.WriteLine("Ср");
+             break;
+           }
+     default:
+      {
+              System.Console.WriteLine("Всего 7 дней в недели! ");
+             break;
+      }
+}
+
+
